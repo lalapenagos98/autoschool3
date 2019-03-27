ef2cffa88f6576ae360c4f59d36fe452226a7973
unknown <lalitapenagos98@hotmail.comgit config --global user.lalapenagos98lanapenagos98git remote add origin https://github.com/lalapenagos98/autoschool.git};cscxzascascsc.q)82>
2019-03-27 11:03:00 -0600

    m

--- model/database.php
+++ model/database.php
@@ -6,7 +6,7 @@ class Database
 	public static function StartUp()
 
 	{
-		$pdo = new PDO('mysql:host=localhost;dbname=autoschool;charset=utf8','root','');
+		$pdo = new PDO('mysql:host=localhost;dbname=autoschool;charset=utf8','root','ixSUyqErTZQma8');
 		$pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	return $pdo;
 	}
